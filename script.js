a87a509665b7737a71a7ccedb68bca27f23430c4
Bheeresh <142218770+Bheeresh-2001@users.noreply.github.com>
2024-01-05 17:39:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const num1 = Math.ceil(Math.random()*10)
+const num1 = Math.ceil(Math.random()*100)
 const num2 = Math.ceil(Math.random()*10)
 
 const questionE1 = document.getElementById("question");
